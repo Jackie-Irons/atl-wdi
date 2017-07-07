bc28e727cc67ef18af0df9bd0dc7bb220e00d894
Jay-77 <jirons2977@gmail.com>
2017-07-06 21:43:30 -0400

    progress bar update

--- homework/unit_01/javaScript/progress-bar/hw.js
+++ homework/unit_01/javaScript/progress-bar/hw.js
@@ -2,17 +2,30 @@
 // Copyright (C) 2016 Matt Brendzel under the GNU General Public License.
 // See LICENSE for details.
 
-var timerUI = {
+vvar timerUI = {
   drawNumericDisplay: function(timerValue){
-    // Your Code Here
+    document.getElementById('numeric-display').textContent = timerValue;
+    var numericDisplay = document.getElementById('numeric-display');
+     numericDisplay.textContent = timerValue;
+     if (timerValue <= 10) {
+      numericDisplay.style.color = 'red';
+       var newSize = (1.5 * Math.pow(1.05, 11 - timerValue)).toFixed(2);
+      numericDisplay.style.fontSize = newSize + 'em';
+     }
   },
   drawProgressBars: function(timerValue){
-    // Your Code Here
+    var progress = 100 - timerValue;
+     var bars = document.getElementsByClassName('progress-bar');
+      for (var i = 0; i < bars.length; i++) {
+      bars[i].style.width = progress + '%';
+     }
   },
+  
+
+   // Your Code Here
   drawLitFuses: function(timerValue){
-    // Your Code Here
+     // Your Code Here
   },
   drawCrawlers: function(timerValue){
-    // Your Code Here
-  }
-};
+    
+}; // Your Code Here
